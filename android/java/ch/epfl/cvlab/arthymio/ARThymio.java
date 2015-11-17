d40f77109c18dea3f54fbdd459801ce16aefa2b2
Pablo Márquez Neila <pmarquezneila@gmail.com>
2015-11-17 03:48:33 +0100

    Receive and draw device orientation

--- android/java/ch/epfl/cvlab/arthymio/ARThymio.java
+++ android/java/ch/epfl/cvlab/arthymio/ARThymio.java
@@ -9,10 +9,14 @@ public class ARThymio
         return n_process(input.nativeObj, output.nativeObj);
     }
     
-    public static int get_rows(Mat mat) {
-        return n_get_rows(mat.nativeObj);
+    public static int process(Mat input, Mat output,
+        Mat deviceOrientation)
+    {
+        return n_process(input.nativeObj, output.nativeObj,
+                         deviceOrientation.nativeObj);
     }
     
     public static native int n_process(long input, long output);
-    public static native int n_get_rows(long mat);
+    public static native int n_process(long input, long output,
+                                       long deviceOrientation);
 }
--- src/arthymio.cpp
+++ src/arthymio.cpp
@@ -14,7 +14,7 @@ static bool mFound = false;
 static GH mGH;
 static ArthymioBlobModel mRobot;
 static IntrinsicCalibration calibration;
-static float mScale = 1.0;
+static float mScale = 0.33;
 
 void drawPointsAndIds(cv::Mat& inputImage, const std::vector<DetectionGH>& matches)
 {
@@ -28,7 +28,27 @@ void drawPointsAndIds(cv::Mat& inputImage, const std::vector<DetectionGH>& match
     }
 }
 
-void loadCalibration(const std::string& filename, IntrinsicCalibration* calibration, const cv::Size* imgSize)
+void drawAxes(cv::Mat& image, const cv::Mat& orientation)
+{
+    static const cv::Scalar axes_colors[] = {cv::Scalar(255, 0, 0), cv::Scalar(0, 255, 0), cv::Scalar(0, 0, 255)};
+    cv::Size::value_type width = image.size().width;
+    cv::Point2d center(width * 0.1, width * 0.1);
+    double length = width * 0.05;
+    
+    for(int i = 0; i < 3; ++i)
+    {
+        const cv::Point2d direction(orientation.at<float>(i, 1), orientation.at<float>(i, 0));
+        const cv::Point2d arrow = center - length * direction;
+        const cv::Scalar& color = axes_colors[i];
+        
+        cv::line(image, center, arrow, color);
+    }
+}
+
+void loadCalibration(const std::string& filename,
+                     IntrinsicCalibration* calibration,
+                     const cv::Size* imgSize,
+                     double scale = 1.0)
 {
     cv::FileStorage fs;
     fs.open(filename, cv::FileStorage::READ);
@@ -38,34 +58,38 @@ void loadCalibration(const std::string& filename, IntrinsicCalibration* calibrat
         throw std::runtime_error("Calibration file not found!");
     }
     
-    readCalibrationFromFileStorage(fs,*calibration);
-    rescaleCalibration(*calibration, *imgSize);
+    readCalibrationFromFileStorage(fs, *calibration);
+    
+    cv::Size targetSize = *imgSize;
+    targetSize.width *= scale;
+    targetSize.height *= scale;
+    rescaleCalibration(*calibration, targetSize);
     
     fs.release();
 }
 
 void init(const cv::Size &imgSize)
 {
-     static const std::string ghfilename = "../data/GH_Arth_Perspective.dat";
-    //static const std::string ghfilename = "/sdcard/GH_Arth_Perspective.dat";
+    // static const std::string ghfilename = "../data/GH_Arth_Perspective.dat";
+    static const std::string ghfilename = "/sdcard/GH_Arth_Perspective.dat";
     mGH.loadFromFile(ghfilename);
     
-    loadCalibration("../data/calibration/embedded_camera_calib.xml", &calibration, &imgSize);
-    //loadCalibration("../data/calibration/nexus_camera_calib.xml", &calibration, &imgSize);
-    ////loadCalibration("/sdcard/nexus_camera_calib.xml", &calibration, &imgSize, mScale);
+    // loadCalibration("../data/calibration/embedded_camera_calib.xml", &calibration, &imgSize);
+    // loadCalibration("../data/calibration/nexus_camera_calib.xml", &calibration, &imgSize);
+    loadCalibration("/sdcard/nexus_camera_calib.xml", &calibration, &imgSize, mScale);
     mGH.setCalibration(calibration);
     
     initialized = true;
 }
 
-int process(const cv::Mat& input, cv::Mat& output)
+int process(const cv::Mat& input, cv::Mat& output,
+            const cv::Mat* deviceOrientation)
 {
     // input.copyTo(output);
     cv::resize(input, output, cv::Size(0, 0), mScale, mScale);
 
     if(!initialized)
         init(output.size());
-    
     
     cv::Affine3d robotPose;
     std::vector<DetectionGH> matches;
@@ -77,10 +101,8 @@ int process(const cv::Mat& input, cv::Mat& output)
     
     drawPointsAndIds(output, matches);
     
+    if(deviceOrientation)
+        drawAxes(output, *deviceOrientation);
+    
     return 0;
 }
-
-// int get_rows(const cv::Mat& m)
-// {
-//     return m.rows;
-// }
